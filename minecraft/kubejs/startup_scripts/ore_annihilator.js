109447ca01a8068a33f4a3cacdcc2b7a1db72cb6
Qweze <notqweze@gmail.com>
2025-07-29 23:28:39 -0400

    more mods
    draconic evoluting gaming

--- minecraft/kubejs/startup_scripts/ore_annihilator.js
+++ minecraft/kubejs/startup_scripts/ore_annihilator.js
@@ -1,7 +1,11 @@
 WorldgenEvents.remove(event => {
     event.removeFeatureById('underground_ores', [
         'create:zinc_ore',
-        'crowns:overworld_uranium_ore'
+        'crowns:overworld_uranium_ore',
+        'draconicevolution:overworld_draconium_ore',
+        'draconicevolution:nether_draconium_ore',
+        'draconicevolution:end_draconium_ore'
+        
     ]);
     event.removeFeatureById('fluid_springs', [
         'gtceu:raw_oil_sprout'
